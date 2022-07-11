66337e958fb468f7ad006905a3629d0788e1087b
N. Martin Abregu <nmabregu@gmail.com>
2022-07-11 15:53:08 -0300

    add login and jwt

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 const User = require('../models/userModel');
 const usersControllers = {
     register: (req, res) => {
@@ -34,7 +35,45 @@ const usersControllers = {
         ;
     },
     login: (req, res) => {
-        res.send("Login");
+        let email = req.body.email;
+        let password = req.body.password;
+        User.findOne({ email: email })
+            .then(user => {
+                if (!user) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+                bcrypt.compare(password, user.password, (err, result) => {
+                    if (err) {
+                        return res.status(401).json({
+                            message: 'Auth failed'
+                        });
+                    }
+                    if (result) {
+                        const token = jwt.sign({
+                            email: user.email,
+                            userId: user._id
+                        },
+                            'secret',
+                            { expiresIn: '1h' }
+                        );
+                        return res.status(200).json({
+                            message: 'Auth successful',
+                            token: token
+                        });
+                    }
+                    res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+                );
+            }).catch(err => {
+                res.status(500).json({
+                    error: err
+                });
+            }
+            );
     }
 }
 
